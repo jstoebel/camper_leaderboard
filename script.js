6f891934f9b73085ed2917dd6aeed9506c5b11f3
Jacob Stoebel <stoebelj@berea.edu>
2016-06-14 20:14:51 -0400

    finished react and styles

--- script.js
+++ script.js
@@ -39,53 +39,39 @@ var Board = React.createClass({
 
     },
 
+    downArrow: function(order) {
+        //returns a span with an arrow this is displayed if this.state.order == order
+    
+        return (
+            <span className={"glyphicon glyphicon-chevron-down"+(this.state.order==order ? "" : "hidden")}></span>
+        )
+    },
+
+    sortbyRecent: function(){
+        //changes state of the order we are sorting by
+
+        this.setState({order: "recent"})
+    },
+
+    sortbyAlltime: function(){
+        //changes state of the order we are sorting by
+
+        this.setState({order: "alltime"})
+    },
+
     eachRecord: function(record, i){
         //render reach record
         return (
             <tr key={i}>
-                <td>{i+1}</td>
-                <td><img src={record.img} alt=""/></td>
-                <td>{record.username}</td>
-                <td>{record.recent}</td>
-                <td>{record.alltime}</td>
+                <td className="text-center vcenter">{i+1}</td>
+                <td>
+                    <img className="user-img img-rounded" src={record.img} alt=""/>
+                    <a className="username" href={"http://www.freecodecamp.com/".concat(record.username)}>{record.username}</a>
+                </td>
+                <td className="text-center vcenter">{record.recent}</td>
+                <td className="text-center vcenter">{record.alltime}</td>
 
             </tr>
-        )
-    },
-
-    renderAlltime: function(){
-        return(
-            <table className="table 
-            table-stripped 
-            table-bordered 
-            table-responsive">
-                <tr>
-                    <th> Rank </th>
-                    <th> Camper Name </th>
-                    <th> Brownie Points: last 30 days </th>
-                    <th> Brownie Points: all time </th> 
-                </tr>
-
-            </table>
-        )
-    },
-
-    renderRecent: function(){
-        return(
-            <table className="table 
-            table-stripped 
-            table-bordered 
-            table-responsive">
-                <tr>
-                    <th> Rank </th>
-                    <th> Camper Name </th>
-                    <th> Brownie Points: last 30 days </th>
-                    <th> Brownie Points: all time </th> 
-                </tr>
-                <trbody>
-                    {this.state.recent.map(this.eachRecord)}
-                </trbody>
-            </table>
         )
     },
 
@@ -93,11 +79,34 @@ var Board = React.createClass({
         console.log(this.state)
         if(this.state.order == "recent"){
             //render recent
-            return this.renderRecent();
+            var records = this.state.recent;
         } else if(this.state.order == "alltime") {
             //render alltime
-            return this.renderAlltime();
+            var records = this.state.alltime;
         }
+
+        return (
+            <table className="table 
+            table-stripped 
+            table-bordered 
+            table-responsive">
+                <tr>
+                    <th> Rank </th>
+                    <th> Camper Name </th>
+                    <th className="score-col"
+                        onClick={this.sortbyRecent}>
+                        Brownie Points: last 30 days {this.downArrow("recent")}
+                    </th>
+                    <th className="score-col"
+                        onClick={this.sortbyAlltime}>
+                        Brownie Points: all time {this.downArrow("alltime")}
+                    </th> 
+                </tr>
+                <tbody>
+                    {records.map(this.eachRecord)}
+                </tbody>
+            </table>
+        )
 
     }
 })
@@ -105,3 +114,6 @@ var Board = React.createClass({
 
 React.render(<Board/>, 
     document.getElementById('container'));
+
+
+// .glyphicon-chevron-down
